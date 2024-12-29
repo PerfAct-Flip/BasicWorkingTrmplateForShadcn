188eb0c60982f2535e295e970830d7e27ad682ec
PerfAct-Flip <sohamsinha208@gmail.com>
2024-12-29 16:26:03 +0530

    ADDING ALL THE FILES

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <div>
+      <div> basic temp for using shadcn </div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
